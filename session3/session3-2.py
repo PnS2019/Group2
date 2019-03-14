ae06d2f4ab13bbce7f3b57af0555d9719b2e5d00
Pascal Schärli <pas.schaerli@sunrise.ch>
2019-03-14 15:47:58 +0100

    Sess 3 Tasks 1 and 2

--- session3/session3-2.py
+++ session3/session3-2.py
@@ -1,4 +1,4 @@
-"""Multi-Layer Perceptron for Fashion MNIST Classification.
+"""Convolutional Neural Network for Fashion MNIST Classification.
 
 Team #name
 """
@@ -7,23 +7,17 @@ from __future__ import print_function, absolute_import
 import numpy as np
 import matplotlib.pyplot as plt
 
-from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.models import Model
 from tensorflow.keras.utils import to_categorical
 
 from pnslib import utils
-from pnslib import ml
-
-import os
-from tensorflow import logging
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-logging.set_verbosity(logging.ERROR)
 
 # Load all the ten classes from Fashion MNIST
 # complete label description is at
 # https://github.com/zalandoresearch/fashion-mnist#labels
 (train_x, train_y, test_x, test_y) = utils.fashion_mnist_load(
-    data_type="full", flatten=True)
+    data_type="full", flatten=False)
 
 num_classes = 10
 
@@ -38,16 +32,6 @@ test_x -= mean_train_x
 
 print("[MESSAGE] Dataset is preprocessed.")
 
-# Use PCA to reduce the dimension of the dataset,
-# so that the training will be less expensive
-# perform PCA on training dataset
-train_X, R, n_retained = ml.pca(train_x)
-
-# perform PCA on testing dataset
-test_X = ml.pca_fit(test_x, R, n_retained)
-
-print("[MESSAGE] PCA is complete.")
-
 # converting the input class labels to categorical labels for training
 train_Y = to_categorical(train_y, num_classes=num_classes)
 test_Y = to_categorical(test_y, num_classes=num_classes)
@@ -55,18 +39,35 @@ test_Y = to_categorical(test_y, num_classes=num_classes)
 print("[MESSAGE] Converted labels to categorical labels.")
 
 # define a model
-input_dim = train_X.shape[1]
 
-x = Input((input_dim,), name="input_layer")
-y = Dense(100, activation="relu", name="linear_layer1")(x)
-y = Dense(100, activation="relu", name="linear_layer2")(y)
-y = Dense(num_classes, activation="softmax", name="output_layer")(y)
+input_dim = train_x.shape[1:]
+
+x = Input(input_dim, name="input_layer")
+
+
+y = Conv2D(filters=20,
+           kernel_size=(7, 7),
+           activation="relu")(x)
+
+y = MaxPooling2D((2, 2),
+                 strides=(2, 2))(y)
+
+y = Conv2D(filters=25,
+           kernel_size=(5, 5),
+           activation="relu")(y)
+
+y = MaxPooling2D((2, 2),
+                 strides=(2, 2))(y)
+
+y = Flatten()(y)
+
+y = Dense(200,
+          activation="relu")(y)
+
+y = Dense(10,
+          activation="softmax")(y)
+
 model = Model(x, y)  # compile the hypothesis function
-
-
-# model = Sequential([Dense(100, input_dim=input_dim, activation="relu"),
-#                     Dense(100, activation="relu"),
-#                     Dense(num_classes, activation="softmax")])
 
 
 print("[MESSAGE] Model is defined.")
@@ -87,23 +88,23 @@ print("[MESSAGE] Model is compiled.")
 
 # train the model with fit function
 # See https://keras.io/models/model/ for usage
-model.fit(x=train_X, y=train_Y,
-          batch_size=64, epochs=100,
-          validation_data=(test_X, test_Y))
+model.fit(x=train_x, y=train_Y,
+          batch_size=64, epochs=10,
+          validation_data=(test_x, test_Y))
 
 print("[MESSAGE] Model is trained.")
 
 # save the trained model
-model.save("mlp-fashion-mnist-trained.hdf5")
+model.save("conv-net-fashion-mnist-trained.hdf5")
 
 print("[MESSAGE] Model is saved.")
 
 # visualize the ground truth and prediction
 # take first 10 examples in the testing dataset
-test_X_vis = test_X[:10]  # fetch first 10 samples
+test_x_vis = test_x[:10]  # fetch first 10 samples
 ground_truths = test_y[:10]  # fetch first 10 ground truth prediction
 # predict with the model
-preds = np.argmax(model.predict(test_X_vis), axis=1).astype(np.int)
+preds = np.argmax(model.predict(test_x_vis), axis=1).astype(np.int)
 
 labels = ["Tshirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal",
           "Shirt", "Sneaker", "Bag", "Ankle Boot"]
@@ -112,7 +113,7 @@ plt.figure()
 for i in range(2):
     for j in range(5):
         plt.subplot(2, 5, i * 5 + j + 1)
-        plt.imshow(test_x[i * 5 + j].reshape(28, 28), cmap="gray")
+        plt.imshow(test_x[i * 5 + j, ..., 0], cmap="gray")
         plt.title("Ground Truth: %s, \n Prediction %s" %
                   (labels[ground_truths[i * 5 + j]],
                    labels[preds[i * 5 + j]]))
