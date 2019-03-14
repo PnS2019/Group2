00592463a95fd8e2df3c8b83463c36b2359d1a08
Pascal Schärli <pas.schaerli@sunrise.ch>
2019-03-14 13:19:06 +0100

    Folder for each session

---
+++ session1/session1.py
@@ -0,0 +1,73 @@
+import numpy as np
+from tensorflow.keras import backend as K
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Exercise 1
+a = K.placeholder(shape=(5,))
+b = K.placeholder(shape=(5,))
+c = K.placeholder(shape=(5,))
+
+e = a**2 + b**2 + c**2 + 2 * b * c
+fun = K.function(inputs=[a, b, c], outputs=(e,))
+
+v_a = 1
+v_b = 2
+v_c = 3
+print("Exercise 1:")
+print("{a}^2 + {b}^2 + {c}^2 + 2 * {a} * {b} = {f}".format(a=v_a, b=v_b, c=v_c, f=fun([v_a, v_b, v_c])[0]))
+print()
+
+# Exercise 2
+x = K.placeholder(shape=())
+
+tanh = (K.exp(x) - K.exp(-x)) / (K.exp(x) + K.exp(-x))
+tanh_function = K.function(inputs=[x], outputs=(tanh,))
+
+grad = K.gradients(loss=tanh, variables=[x])
+grad_function = K.function(inputs=[x], outputs=(grad[0],))
+
+values = [-100, 10, 0, 10, 100]
+
+print("Exercise 2:")
+for value in values:
+    print("x = {}:".format(value))
+    print("\ttanh = {}".format(tanh_function([value])))
+    print("\tgrad = {}".format(grad_function([value])))
+    print()
+
+# Exercise 3
+w = K.placeholder(shape=(2,))
+b = K.placeholder(shape=(1,))
+x = K.placeholder(shape=(2,))
+
+z = 1 / (1 + K.exp(w[0] * x[0] + w[1] * x[1] + b))
+
+f = K.function(inputs=[w, b, x], outputs=(z,))
+
+v_w = np.ones(shape=(2,))
+v_b = np.ones(shape=(1,))
+v_x = np.ones(shape=(2,))
+
+print("Exercise 3:")
+for mul in range(10):
+    print("mul = {} : {}".format(mul, f([mul * v_w, mul * v_b, mul * v_x])))
+print()
+
+# Exercise 4
+
+x = K.placeholder(shape=())
+n = 4
+
+eq = 0
+
+variables = []
+for i in range(n + 1):
+    variables.append(K.placeholder(shape=()))
+    eq += variables[-1] * x**i
+
+grad = K.gradients(loss=eq, variables=variables)
+
+print("Exercise 4:")
+print(grad)
