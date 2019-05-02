0fdf5ec759ad989b910d7916323ff53e06173410
Pascal Schärli <pas.schaerli@sunrise.ch>
2019-05-02 14:55:37 +0200

    Formatting

--- project/raspberry_cam.py
+++ project/raspberry_cam.py
@@ -9,19 +9,22 @@ import string
 
 
 def say_text(text):
-  """Speaks a text over the speaker"""
-  speaker = ResponsiveVoice(rate=.5, vol=1)
-  speaker.say(text, gender="male", lang="en-GB")
+    """Speaks a text over the speaker"""
+    speaker = ResponsiveVoice(rate=.5, vol=1)
+    speaker.say(text, gender="male", lang="en-GB")
+
+
+model = load_model('models/letclass_valacc0.921.hdf5')
+model.compile()
 
 
 def get_text(letters):
-  model = load_model('my_model.h5')
-  model.compile()
-  preds = np.argmax(model.predict(letters), axis=1).astype(np.int)
-  out = ""
-  for letter_index in preds:
-    out += string.ascii_lowercase[letter_index]
-  return out
+    global model
+    preds = np.argmax(model.predict(letters), axis=1).astype(np.int)
+    out = ""
+    for letter_index in preds:
+        out += string.ascii_lowercase[letter_index]
+    return out
 
 
 
@@ -36,15 +39,15 @@ time.sleep(0.1)
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-  # grab the raw NumPy array representing the image, then initialize the timestamp
-  # and occupied/unoccupied text
-  image = frame.array
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
 
-  # Display the resulting frame
-  cv2.imshow('frame', image)
-  # the loop breaks at pressing `q`
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+    # Display the resulting frame
+    cv2.imshow('frame', image)
+    # the loop breaks at pressing `q`
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-  # clear the stream in preparation for the next frame
-  rawCapture.truncate(0)
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
